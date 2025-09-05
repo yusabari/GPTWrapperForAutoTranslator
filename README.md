8e12cdb666e8bb0b84577d1ec846cf4894051a37
yusabari <yumesaki.kr@gmail.com>
2025-09-05 11:19:02 +0900

    update main changes

--- README.md
+++ README.md
@@ -1,22 +1,26 @@
-# ezTransWeb
-이지트랜스를 웹 API 형태로 쓰기 위해 만들어진 Python 스크립트입니다.
+# GPTWrapperForAutoTranslator
+ChatGPT 번역을 웹 API 형태로 쓰기 위해 만들어진 Python 스크립트입니다.
 
 [AutoTranslator](https://github.com/bbepis/XUnity.AutoTranslator)등의 사용자 정의 Endpoint 지정 가능한 번역 플러그인에 사용할 용도로 만들어졌습니다.
 # 필수 환경
 * 이지트랜스 (최소 한번 실행 필수)
-* [꿀도르님 개조 Ehnd](https://blog.naver.com/waltherp38/221062272423)
-* Python 3.x **32비트**
+* Python
 * Flask
 
 # 주의
 * 해당 스크립트를 절대 외부망으로 노출하거나 서비스 하지 마시기 마랍니다. 보안 문제가 있을 수 있습니다.
 
 # 사용 방법
-필수 환경이 다 갖춰진 상태라면, 명령 프롬프트에서 `python eztrans.py`로 실행할 수 있습니다.
+실행 전 trans.py 파일을 수정하여 API_KEY 및 TRANSLATION_PROMPT 를 수정해주세요.
+필수 환경이 다 갖춰진 상태라면, 명령 프롬프트에서 `python trans.py`로 실행할 수 있습니다.
 
 기본값은 127.0.0.1을 호스트로, 5000번 포트에서 동작합니다.
-# 자세한 사용 방법
-HOWTO.md 파일을 참고해 주세요.
+# XUnity.AutoTranslator와 사용
+다음과 같은 커스텀 엔드포인트를 추가해주세요.
+```
+Endpoint=CustomTranslate
+[Custom]
+Url=http://127.0.0.1:5000/translate
+```
 # Special Thanks to
-* [Ehnd](https://github.com/sokcuri/ehnd)
-* [Anemone](https://github.com/sokcuri/anemone)
+* [ezTransWeb](https://github.com/HelloKS/ezTransWeb)
--- trans.py
+++ trans.py
@@ -1,27 +1,22 @@
-# -*- coding: utf-8 -*-
-from ctypes import WinDLL, c_char_p, c_int, c_wchar_p
-from ctypes.wintypes import BOOL
+API_KEY = "" # Insert your OpenAI API key here
+TRANSLATION_PROMPT = "게임을 번역 중입니다. 다음 문장을 한국어로 번역하십시오. 사족을 붙이지 말고 번역된 문장만 출력하십시오.: "
+
 from flask import Flask, request
+from openai import OpenAI  
 import re
 
 app = Flask(__name__)
 
 class TransEngine:
-    def initialize(self, engine):
-        self.start = engine.J2K_InitializeEx
-        self.start.argtypes = [c_char_p, c_char_p]
-        self.start.restype = BOOL
+    def translate(self, src_text):
+        trans_obj = client.chat.completions.create(
+            model = "gpt-4o-mini",
+            messages = [{"role" : "user", "content" : TRANSLATION_PROMPT + src_text}]
+        )
+        return trans_obj.choices[0].message.content
 
-        self.trans = engine.J2K_TranslateMMNTW
-        self.trans.argtypes = [c_int, c_wchar_p]
-        self.trans.restype = c_wchar_p
-
-        self.start_obj = self.start(b"CSUSER123455", b"C:\Program Files (x86)\ChangShinSoft\ezTrans XP\Dat")
-    def translate_j2k(self, src_text):
-        trans_obj = self.trans(0, src_text)
-        return trans_obj
-        
-eng = TransEngine()
+client = OpenAI(api_key=API_KEY)   
+engine = TransEngine()
 
 def decode_text(txt):
     chars = "↔◁◀▷▶♤♠♡♥♧♣⊙◈▣◐◑▒▤▥▨▧▦▩♨☏☎☜☞↕↗↙↖↘♩♬㉿㈜㏇™㏂㏘＂＇∼ˇ˘˝¡˚˙˛¿ː∏￦℉€㎕㎖㎗ℓ㎘㎣㎤㎥㎦㎙㎚㎛㎟㎠㎢㏊㎍㏏㎈㎉㏈㎧㎨㎰㎱㎲㎳㎴㎵㎶㎷㎸㎀㎁㎂㎃㎄㎺㎻㎼㎽㎾㎿㎐㎑㎒㎓㎔Ω㏀㏁㎊㎋㎌㏖㏅㎭㎮㎯㏛㎩㎪㎫㎬㏝㏐㏓㏃㏉㏜㏆┒┑┚┙┖┕┎┍┞┟┡┢┦┧┪┭┮┵┶┹┺┽┾╀╁╃╄╅╆╇╈╉╊┱┲ⅰⅱⅲⅳⅴⅵⅶⅷⅸⅹ½⅓⅔¼¾⅛⅜⅝⅞ⁿ₁₂₃₄ŊđĦĲĿŁŒŦħıĳĸŀłœŧŋŉ㉠㉡㉢㉣㉤㉥㉦㉧㉨㉩㉪㉫㉬㉭㉮㉯㉰㉱㉲㉳㉴㉵㉶㉷㉸㉹㉺㉻㈀㈁㈂㈃㈄㈅㈆㈇㈈㈉㈊㈋㈌㈍㈎㈏㈐㈑㈒㈓㈔㈕㈖㈗㈘㈙㈚㈛ⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓨⓩ①②③④⑤⑥⑦⑧⑨⑩⑪⑫⑬⑭⑮⒜⒝⒞⒟⒠⒡⒢⒣⒤⒥⒦⒧⒨⒩⒪⒫⒬⒭⒮⒯⒰⒱⒲⒳⒴⒵⑴⑵⑶⑷⑸⑹⑺⑻⑼⑽⑾⑿⒀⒁⒂"
@@ -34,19 +29,16 @@ def encode_text(txt):
     return re.sub(r'(?i)(?<!\\)(?:\\\\)*\\u([0-9a-f]{4})', lambda m: chr(int(m.group(1), 16)), txt)
 
 def main():
-    engine_object = WinDLL('C:\Program Files (x86)\ChangShinSoft\ezTrans XP\J2KEngineH.dll')
-    eng.initialize(engine_object)
-    
     app.run(host="127.0.0.1", port=5000, threaded=True)
 
 @app.route("/")
 def home():
-    return "ezTranslator J2K Web Wrapper"
+    return "LLM Translation Web Wrapper"
 
 @app.route("/translate")
 def webtranslate():
     src_text = request.args.get('text')
-    return encode_text(eng.translate_j2k(decode_text(src_text)))
+    return encode_text(engine.translate(decode_text(src_text)))
 
 if __name__ == '__main__':
     main()
